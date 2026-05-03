4f8df47d3fd16e7ae244b5d5f54d3760236016b5
pinpanel237 <pinpanel237@gmail.com>
2026-05-03 14:33:36 +0900

    [level 2] Title: 방문 길이, Time: 0.64 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -0,0 +1,26 @@
+def solution(dirs):
+    dir_set = set()
+    cur_pos = (0, 0)
+    next_pos = (-6, -6)
+    
+    for dir in dirs:
+        x, y = cur_pos
+        if dir == 'U':
+            next_pos = (x, y + 1)
+        elif dir == 'D':
+            next_pos = (x, y - 1)
+        elif dir == 'L':
+            next_pos = (x - 1, y)
+        elif dir == 'R':
+            next_pos = (x + 1, y)
+        
+        if is_in_boundary(next_pos):
+            dir_set.add((cur_pos, next_pos))
+            dir_set.add((next_pos, cur_pos))
+            cur_pos = next_pos
+    
+    return len(dir_set) // 2
+
+def is_in_boundary(next_pos):
+    x, y = next_pos
+    return -5 <= x <= 5 and -5 <= y <= 5
